1e017d91294b693e6b8a8b068732056f731269a8
Priyesh <priyeshchaudhari35@gmail.com>
2025-02-06 08:46:45 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Download from './sections/Download'
+import Faq from './sections/Faq'
 import Features from './sections/Features'
+import Footer from './sections/Footer'
 import Header from './sections/Header'
 import Hero from './sections/Hero'
 import Pricing from './sections/Pricing'
+import Testimonial from './sections/Testimonial'
 
 function App() {
   
@@ -14,6 +18,10 @@ function App() {
         <Hero />
         <Features />
         <Pricing />
+        <Faq />
+        <Testimonial />
+        <Download />
+        <Footer />
       </main>
     </>
   )
